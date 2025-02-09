78d85e6df29081fba0ef2890961364e1304e3874
Jose Luna <lunajose6969@gmail.com>
2025-02-09 13:41:59 -0600

    feat: Project created

---
+++ search-debouncer/search-debouncer/search_debouncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  search_debouncerApp.swift
+//  search-debouncer
+//
+//  Created by Jose Luna on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct search_debouncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
