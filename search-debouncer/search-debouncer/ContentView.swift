3774ba5c29e73816fd9ea2147f9c81dd231764f0
Bryan Luna <111804230+Rexmoon@users.noreply.github.com>
2025-02-09 13:44:56 -0600

    001: Project created (#1)
    feat: Project created

---
+++ search-debouncer/search-debouncer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  search-debouncer
+//
+//  Created by Jose Luna on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
